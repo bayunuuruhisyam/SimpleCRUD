5fb4e4f4b36904d73fd696fa5dd2a39d3ecfb60d
bayun <bayun@BAYU>
2026-02-03 08:15:12 +0700

    Add project files.

---
+++ SimpleCRUD/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using SimpleCRUD.Components;
+using SimpleCRUD.DataAccess;
+using MudBlazor.Services;
+using SimpleCRUD.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddDbContext<AppDBContext>(Option =>
+{
+    Option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddMudServices();
+builder.Services.AddScoped<EmployeeService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
---
+++ SimpleCRUD/Services/EmployeeService.cs
@@ -0,0 +1,58 @@
+﻿using SimpleCRUD.ViewModels;
+using SimpleCRUD.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using SimpleCRUD.DataAccess.Entities;
+
+namespace SimpleCRUD.Services
+{
+    public class EmployeeService
+    {
+        private readonly AppDBContext dBContext;
+
+        public EmployeeService(AppDBContext dBContext)
+        {
+            this.dBContext = dBContext;
+        }
+
+        public async Task<List<EmployeeViewModel>> GetAllEmployees()
+        {
+            return await dBContext.Employees
+                .OrderBy(x => x.FullName)
+                .Select(x => new EmployeeViewModel
+                {
+                    EmployeeId = x.EmployeeId,
+                    FullName = x.FullName,
+                    Department = x.Department,
+                    DateOfBirth = x.DateOfBirth,
+                    Age = x.Age,
+                    PhoneNumber = x.PhoneNumber
+                }).ToListAsync();
+        }
+            
+        public async Task<bool> CreateNewEmployee(EmployeeViewModel model)
+        {
+            try
+            {
+                Employee employee = new Employee
+                {
+                    FullName = model.FullName,
+                    Department = model.Department,
+                    DateOfBirth = model.DateOfBirth,
+                    Age = model.Age,
+                    PhoneNumber = model.PhoneNumber,
+
+                };
+
+                dBContext.Employees.Add(employee);
+
+                var result = dBContext.SaveChanges();
+
+                return result > 0;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }   
+    }
+}
